2e1d5ee2e2ee4188fbe873a03a34172a3348ad1e
Tristan Kuehn <tkuehn@uwo.ca>
2023-09-20 14:23:13 -0400

    Make output_type a tuple
    This is a bug that heudiconv used to silently fix but doesn't anymore,
    so it causes problem with new versions of heudiconv.

--- heuristics/cfmm_base.py
+++ heuristics/cfmm_base.py
@@ -1,6 +1,6 @@
 import os
 
-def create_key(template, outtype=('nii.gz'), annotation_classes=None):
+def create_key(template, outtype=('nii.gz',), annotation_classes=None):
     if template is None or not template:
         raise ValueError('Template must be a valid format string')
     return (template, outtype, annotation_classes)
